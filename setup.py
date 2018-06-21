bfca8c42b0888ec01e04f3bac1109adaeec1391b
Daniel Serrano <dserrano1983@gmail.com>
2018-06-21 05:47:43 +0200

    1st commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2017_C_yahoo_ks_datareader',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/manoelgadi/pythonpackagesample',
+      author='Prof. Manoel Gadi',
+      author_email='mfalonso@faculty.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2017_C_yahoo_ks_datareader'],
+      zip_safe=False)
